f31522647e246bb9cff03702948bcb3173ecb7f6
anujpradeep <pradeepanuj123@gmail.com>
2022-02-03 13:55:51 -0700

    Added timings for both stream and parallel stream

--- src/PickShareFunctional.java
+++ src/PickShareFunctional.java
@@ -19,4 +19,5 @@ public class PickShareFunctional {
 										.toList();
 		return shareInfo.get(0);
 	}
+
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,15 @@
-
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("%d", PickShareFunctional.findHighPriced(Shares.symbols.stream()).price.intValue());
-	}
+		long streamStart = System.currentTimeMillis();
+		System.out.printf("%d\n", PickShareFunctional.findHighPriced(Shares.symbols.stream()).price.intValue());
+		long streamFinish = System.currentTimeMillis();
+		long parallelStart = System.currentTimeMillis();
+		System.out.printf("%d\n", PickShareFunctional.findHighPriced(Shares.symbols.parallelStream()).price.intValue());
+		long parallelFinish = System.currentTimeMillis();
 
+		System.out.println("Stream time(ms) = " + (streamFinish-streamStart));
+		System.out.println("Parallel Stream time(ms) = " + (parallelFinish-parallelStart));
+	}
 }
