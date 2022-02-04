d5a86f5ce121f7a53583f75f986bba729fa06664
Ralph Milford <rmilford@ualberta.ca>
2022-02-03 19:03:56 -0700

    Fixed pickShareFunctional and ShareUtil

--- src/PickShareFunctional.java
+++ src/PickShareFunctional.java
@@ -1,19 +1,29 @@
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
-import java.util.stream.Collectors;
 
 // https://belief-driven-design.com/functional-programming-with-java-exception-handling-e69997c11d3/
 
 public class PickShareFunctional {
-	public static final int MIN_PRICE = 500;
+	private static final BigDecimal MIN_PRICE = new BigDecimal(500);
 	
-	public static ShareInfo findHighPriced(Stream<String> shares) {
-		List<ShareInfo> shareInfo = shares.map(share -> new ShareInfo((String) share, APIFinance.getPrice((String) share)))
+	public static Optional<ShareInfo> findHighPriced(Stream<String> shares) {
+		Optional<ShareInfo> shareInfo = shares.map(share -> { 
+																try {
+																		TimeUnit.SECONDS.sleep(12);
+																} catch (InterruptedException e) {
+																	// TODO Auto-generated catch block
+																	e.printStackTrace();
+																}
+																return new ShareInfo((String) share, APIFinance.getPrice((String) share));})
 										.filter(share -> share.price.isPresent())
-										.filter(share -> share.price.get().intValue() > MIN_PRICE)
-										.sorted((share1, share2) -> share2.price.get().compareTo(share1.price.get()))
-										.collect(Collectors.toList());
-										// .toList();
-		return shareInfo.get(0);
+										.filter(share -> share.price.get().compareTo(MIN_PRICE) > 0)
+										.reduce((share1, share2) -> ShareUtil.pickHigh(share1, share2));
+		return shareInfo;
+	}
+	
+	public static final BigDecimal getMinPrice() {
+		return MIN_PRICE;
 	}
 }
--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("%d", PickShareFunctional.findHighPriced(Shares.symbols.stream()).price.intValue());
+		System.out.printf("%d", PickShareFunctional.findHighPriced(Shares.symbols.stream()).get().price.get().intValue());
 	}
 
 }
