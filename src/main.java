49b6da42b9768d789c47290625b2505297d571fd
naviza <43558584+naviza@users.noreply.github.com>
2022-02-03 18:48:24 -0700

    Add Delete main program

--- src/main.java
+++ src/main.java
@@ -1,9 +1,8 @@
-
-public class MainProg {
+public class main {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.printf("%d", PickShareFunctional.findHighPriced(Shares.symbols.stream()).price.intValue());
-		System.out.println("Hello World, Java app");
 	}
 
 }
