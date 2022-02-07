2c8f8c7237963fb3210adba788dad207dbfdeaf8
naviza <43558584+naviza@users.noreply.github.com>
2022-02-07 09:39:04 -0700

    Cleaned up tests

--- src/main.java
+++ src/main.java
@@ -1,11 +1,10 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class main {
 
 	public static void main(String[] args) {		
-
-		extraTests();
 		
 		long streamStart = System.currentTimeMillis();
 		System.out.println(PickShareFunctional.findHighPriced(Shares.symbols.stream()).get().price.get().intValue());
@@ -17,6 +16,8 @@ public class main {
 		long parallelEnd = System.currentTimeMillis();
 		
 		System.out.println(parallelEnd-parallelStart);
+
+		// extraTests();
 	
 	}
 
@@ -26,15 +27,23 @@ public class main {
 		System.out.println("Empty string Price: " + APIFinance.getPrice("").toString());
 
 		List<String> testList = Arrays.asList("SHOP", "TSLA");
-		System.out.println(PickShareFunctional.findHighPriced(testList.stream()).get().price.get().intValue());
+		System.out.println("{SHOP, TSLA}: " +
+			PickShareFunctional.findHighPriced(testList.stream()).get().price.get().toString());
 
 		testList = Arrays.asList();
-		System.out.println(PickShareFunctional.findHighPriced(testList.stream()).get().price.toString());
+		System.out.println("{}: " +
+			PickShareFunctional.findHighPriced(testList.stream()).toString()
+			);
 		
 		testList = Arrays.asList("SHOP");
-		System.out.println(PickShareFunctional.findHighPriced(testList.stream()).get().price.get().intValue());
+		System.out.println("{SHOP}: " +
+			PickShareFunctional.findHighPriced(testList.stream()).get().price.toString()
+			);
 		
 		System.out.println(PickShareFunctional.findHighPriced(Shares.symbols.stream()).get().price.get().intValue());
+		List<String> noAmazon = new ArrayList<String>(Shares.symbols); noAmazon.remove("AMZN");
+		System.out.println(
+			PickShareFunctional.findHighPriced(noAmazon.stream()).get().price.get().intValue()
+			);
 	}
-
 }
