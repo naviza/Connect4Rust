1f6f417b5af5b5594dbf8c05b2dd2c145108a6df
anujpradeep <pradeepanuj123@gmail.com>
2022-02-06 18:26:01 -0700

    Added elapsed time for both stream and parallel stream

--- src/main.java
+++ src/main.java
@@ -2,7 +2,21 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("%d", PickShareFunctional.findHighPriced(Shares.symbols.stream()).get().price.get().intValue());
+		
+		System.out.println(PickShareFunctional.findHighPriced(Shares.symbols.stream()).get().price.get().intValue());
+		
+
+		long streamStart = System.currentTimeMillis();
+		PickShareFunctional.findHighPriced(Shares.symbols.stream());
+		long streamEnd = System.currentTimeMillis();
+		System.out.println((streamEnd-streamStart));
+
+		long parallelStart = System.currentTimeMillis();
+		PickShareFunctional.findHighPriced(Shares.symbols.parallelStream());
+		long parallelEnd = System.currentTimeMillis();
+
+		System.out.println(parallelEnd-parallelStart);
+	
 	}
 
 }
