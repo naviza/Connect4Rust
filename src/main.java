fd6401abb14a9c435fb0e9bdf5255281f60ad3cd
naviza <43558584+naviza@users.noreply.github.com>
2022-02-07 21:40:24 -0700

    Fixed test code

--- src/main.java
+++ src/main.java
@@ -32,16 +32,30 @@ public class main {
         // extraTests();    
     }
 
-//    private static void extraTests(){
-//        System.out.println("AMZN Price: " + APIFinance.getPrice("AMZN").get().toString());
-//        System.out.println("ABCDEFG Price: " + APIFinance.getPrice("ABCDEFG").toString());
-//        System.out.println("Empty string Price: " + APIFinance.getPrice("").toString());
-//
-//        List<String> testList = Arrays.asList("SHOP", "TSLA");
-//        System.out.println("{SHOP, TSLA}: " +
-//            PickShareFunctional.findHighPriced(testList.stream()).get().price.get().toString());
-//
-//        System.out.println("Stream time(ms) = " + (streamFinish-streamStart));
-//        System.out.println("Parallel Stream time(ms) = " + (parallelFinish-parallelStart));
-//    }
+	private static void extraTests(){
+		System.out.println("AMZN Price: " + APIFinance.getPrice("AMZN").toString());
+		System.out.println("ABCDEFG Price: " + APIFinance.getPrice("ABCDEFG").toString());
+		System.out.println("Empty string Price: " + APIFinance.getPrice("").toString());
+
+		List<String> testList = Arrays.asList("SHOP", "TSLA");
+		System.out.println("{SHOP, TSLA}: " +
+			PickShareFunctional.findHighPriced(testList.stream()).get().toString()
+			);
+
+		testList = Arrays.asList();
+		System.out.println("{}: " +
+			PickShareFunctional.findHighPriced(testList.stream()).toString()
+			);
+		
+		testList = Arrays.asList("SHOP");
+		System.out.println("{SHOP}: " +
+			PickShareFunctional.findHighPriced(testList.stream()).get().toString()
+			);
+		
+		System.out.println(PickShareFunctional.findHighPriced(Shares.symbols.stream()).get().price.get().intValue());
+		List<String> noAmazon = new ArrayList<String>(Shares.symbols); noAmazon.remove("AMZN");
+		System.out.println(
+			PickShareFunctional.findHighPriced(noAmazon.stream()).get()
+			);
+	}
 }
