3e0b5e3ee890c72c4fec87d317934c45ed39c079
anujpradeep <pradeepanuj123@gmail.com>
2022-03-01 17:12:16 -0700

    Update main.rs

--- GP2/redblacktree/src/main.rs
+++ GP2/redblacktree/src/main.rs
@@ -1,7 +1,7 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum NodeColor {
     Red,
     Black,
@@ -10,6 +10,7 @@ enum NodeColor {
 type Tree = Rc<RefCell<TreeNode<u32>>>;
 type RedBlackTree= Option<Tree>;
 
+#[derive(Clone)]
 struct TreeNode<T> {
     pub color: NodeColor,
     pub data: T,
@@ -22,7 +23,7 @@ impl<T> TreeNode<T> {
 	
 	pub fn new_node(t: T) -> TreeNode<T> {
 		TreeNode {
-			color: NodeColor::Black,
+			color: NodeColor::Red,
 			data: t,
 			parent: None,
 			left: None,
@@ -31,8 +32,31 @@ impl<T> TreeNode<T> {
 	}
 }
 
-fn new_rb(t: u32) -> RedBlackTree {
-	Some(Rc::new(RefCell::new(TreeNode::new_node(t))))
+impl NodeColor {
+    fn is_red(self) -> bool{
+        match self {
+            NodeColor::Black => false,
+            NodeColor::Red => true,
+        }
+    }
+
+    fn flip_color(self) -> Self{
+        match self {
+            NodeColor::Black => NodeColor::Red,
+            NodeColor::Red => NodeColor::Black,
+        }
+    }
+}
+
+fn new_rb(t: u32,if_root : bool) -> RedBlackTree {
+    if if_root {
+        let node = TreeNode::new_node(t);
+        node.color.flip_color();
+        let root = Some(Rc::new(RefCell::new(node)));
+        return root;
+    } else {
+        Some(Rc::new(RefCell::new(TreeNode::new_node(t))))
+    }
 }
 
 //fn assign(parent: &mut RedBlackTree, child: &mut RedBlackTree) {
@@ -46,39 +70,86 @@ fn new_rb(t: u32) -> RedBlackTree {
 //	}
 //}
 
-fn rebalance(tree: &mut RedBlackTree) {
-	match tree {
+fn violations(tree: &mut RedBlackTree) {
+
+}
+
+fn right_rotate() {
+
+}
+
+fn left_rotate() {
+
+}
+
+fn balance_tree(tree: &mut Tree) {
+    match &tree.borrow().parent {
+        //if it is None, that means its the root
+        Some(parent_node) => {
+            //node is the parent
+            //have to check the color of the parent
+            if parent_node.borrow().color.is_red() {
+                // have to check if the "uncle" is a red node
+            }
+        },
+        None => tree.borrow().color = tree.borrow().color.flip_color(),
+    }
+}
+
+
+fn in_order_transversal(tree: &RedBlackTree) {
+    match tree {
 		Some(node) => {
-			
-			
-			rebalance(&mut node.borrow_mut().left);
-			rebalance(&mut node.borrow_mut().right);
+			in_order_transversal(&node.borrow().left);
+            println!("{}", node.borrow().data);
+			in_order_transversal(&node.borrow().right);
 		}
-		None => {}
-	};
+		None => println!("Null Leaf"),
+	}
 }
 
 fn insert(tree: &mut RedBlackTree, t: u32) -> RedBlackTree {
-		match tree {
-			Some(root) => {
-				let node = insert(&mut root.borrow_mut().left, t);
-				root.borrow_mut().left = node;
-				match &mut root.borrow_mut().left {
-					Some(child) => child.borrow_mut().parent = Some(root.clone()),
-					None => println!("Error: Failed to insert node")
-				};
-				Some(root.clone())
-			}
-			None => new_rb(t)
-		}
+    match tree {
+        Some(root) => {
+            let node;
+            if root.borrow().data > t {
+                node = insert(&mut root.borrow_mut().left, t);
+                root.borrow_mut().left = node;
+                match &mut root.borrow_mut().left {
+                    Some(child) => {
+                        child.borrow_mut().parent = Some(root.clone());
+                        balance_tree(child); 
+                    },
+                    None => println!("Error: Failed to insert node")
+                };
+            } else {
+                node = insert(&mut root.borrow_mut().right, t);
+                root.borrow_mut().right = node;
+                match &mut root.borrow_mut().right {
+                    Some(child) => {
+                        child.borrow_mut().parent = Some(root.clone());
+                    },
+                    None => println!("Error: Failed to insert node")
+                };
+            }
+            Some(root.clone())
+
+        }
+        None => new_rb(t,false)
+    }
 }
 
 fn delete(tree: &mut RedBlackTree, t: u32) {
 	match tree {
 		Some(root) => {
-			
+			if root.borrow().data == t {
+				
+			} else {
+				delete(&mut root.borrow_mut().left, t);
+				delete(&mut root.borrow_mut().right, t);
+			}
 		}
-		None =>{}
+		None => {}
 	}
 }
 
@@ -116,6 +187,23 @@ fn count_leaves(tree: &RedBlackTree) -> u32 {
 	}
 }
 
+fn count_height(tree: &RedBlackTree) -> u32 {
+	let mut height = 1;
+	match tree {
+		Some(node) => {
+			let left = count_height(&node.borrow().left);
+			let right = count_height(&node.borrow().right);
+			if left >= right {
+				height += left;
+			} else {
+				height += right;
+			}
+			height
+		}
+		None => 0
+	}
+}
+
 fn print_tree(tree: &RedBlackTree) {
 	match tree {
 		Some(leaf) => {
@@ -123,15 +211,25 @@ fn print_tree(tree: &RedBlackTree) {
 			print_tree(&leaf.borrow().left);
 			print_tree(&leaf.borrow().right);
 		}
-		None => println!("End of Tree")
+		None => println!("NULL Leaf")
 	};
 }
 
-fn main() {
-    let mut t: RedBlackTree = new_rb(5);
+fn test_tree() {
+    let mut t: RedBlackTree = new_rb(5,true);
     insert(&mut t, 10);
     insert(&mut t, 20);
+    println!("Printing Tree");
     print_tree(&t);
+    println!();
     println!("Number of nodes: {}", size(&t));
     println!("Number of leaves: {}", count_leaves(&t));
+    println!();
+    println!("Printing tree in-order transversal");
+    in_order_transversal(&t);
+    println!();
+}
+
+fn main() {
+    test_tree();
 }
