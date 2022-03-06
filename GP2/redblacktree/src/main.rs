2899b3ed5ca912b8470f9948fe1c13ee4ccb99eb
anujpradeep <pradeepanuj123@gmail.com>
2022-03-06 16:51:47 -0700

    Almost done with balancing the tree

--- GP2/redblacktree/src/main.rs
+++ GP2/redblacktree/src/main.rs
@@ -59,103 +59,169 @@ fn new_rb(t: u32,if_root : bool) -> RedBlackTree {
 	}
 }
 
-//fn assign(parent: &mut RedBlackTree, child: &mut RedBlackTree) {
-//	match child {
-//		Some(node) => node.borrow_mut().left = parent.clone(),
-//		None => println!("Error: Failed to assign parent to child")
-//	};
-//	match parent {
-//		Some(node) => node.borrow_mut().left = child.clone(),
-//		None => println!("Error: Failed to assign child to parent")
-//	}
-//}
-
-fn violations(tree: &mut RedBlackTree) {
-
+fn right_rotate(grandparent_tree : &mut RedBlackTree) -> RedBlackTree{
+	// make grandparent is right child of the parent. right child of parent is the left child of the grandparent
+	match grandparent_tree {
+		Some(grandparent) => {
+			let parent = grandparent.borrow_mut().parent.clone();
+			grandparent.borrow_mut().parent = grandparent.borrow_mut().left.clone();
+			match grandparent.borrow_mut().parent.clone() {
+				Some(left_child) => {
+					grandparent.borrow_mut().left = left_child.borrow_mut().right.clone();
+					left_child.borrow_mut().right = left_child.borrow_mut().parent.clone();
+					left_child.borrow_mut().parent = parent;
+					grandparent.borrow_mut().parent.clone()
+				},
+				None => panic!("there should be a left child")
+			}
+		},
+		None=> panic!("Should be a grandparent")
+	}
 }
 
-fn right_rotate() {
-
+fn left_rotate(node : &mut RedBlackTree) -> RedBlackTree{
+	// must make grandparent is right child of the parent. right child of parent is the left child of the grandparent
+	match node {
+		Some(grandparent) => {
+			let parent = grandparent.borrow_mut().parent.clone();
+			grandparent.borrow_mut().parent = grandparent.borrow_mut().right.clone();
+			match grandparent.borrow_mut().parent.clone() {
+				Some(right_child) => {
+					grandparent.borrow_mut().right = right_child.borrow_mut().left.clone();
+					right_child.borrow_mut().left = right_child.borrow_mut().parent.clone();
+					right_child.borrow_mut().parent = parent;
+					grandparent.borrow_mut().parent.clone()
+				},
+				None => panic!("there should be a right child")
+			}
+		},
+		None=> panic!("Should be a grandparent")
+	}
 }
 
-fn left_rotate() {
 
-}
-
-fn find_parent() {
-
-}
-
-fn find_grandparent() {
-
-}
-
-fn find_uncle() {
-
+fn check_rotation(node : &mut RedBlackTree) -> i32{
+	match node.clone() {
+		Some(i_node) => {
+			let x = i_node.borrow_mut().parent.clone(); 
+			match i_node.borrow_mut().parent.clone() {
+				Some(ref parent) =>{
+					let y = parent.borrow_mut().parent.clone();
+					match y.clone() {
+						Some(grandparent) => {
+							if grandparent.borrow_mut().left == i_node.borrow_mut().parent.clone() {
+								if parent.borrow_mut().left == node.clone() {
+									return 1;
+								} else {
+									return 2;
+								}
+							} else {
+								if parent.borrow_mut().right == node.clone() {
+									return 3;
+								} else {
+									return 4;
+								}
+							}
+						},
+						None => {
+							//inserted node's parent is the the root
+							panic!("If there is not grandparent, simple recoloring will work");
+						}
+					}
+				}
+				None => {
+					// the inserted node's parent is none, meaning inserted node is the root
+					// This is not possible as in balance_tree() we check if the inserted node is the root
+					panic!("inserted node is the root, which is not possible!");
+				}
+			}
+		},
+		None => {
+			// means the inserted node is none, which is not possible
+			//println!("Something went wrong when inserting the node in the tree");
+			panic!("Something went wrong when inserting the node in the tree")
+		},
+	}
 }
 
 fn balance_tree(tree: &mut RedBlackTree) -> RedBlackTree{
-	match tree {
+	match tree.clone() {
 		Some(node) => {
 			match node.borrow_mut().parent.clone() {
 				Some(parent) => {
-					if parent.borrow_mut().color.is_red() {
+					if parent.borrow().color.is_red() {
 						match parent.borrow_mut().parent.clone() {
 							Some(grandparent) => {
-								if grandparent.borrow_mut().left == node.borrow_mut().parent {
+								if grandparent.borrow_mut().left == node.borrow_mut().parent.clone() {
 									//check right
-									match grandparent.borrow_mut().right.clone() {
-										Some(uncle) => {
-											if uncle.borrow_mut().color.is_red() {
-												//change to black
-												uncle.borrow_mut().color.flip_color();
-												parent.borrow_mut().color.flip_color();
-												grandparent.borrow_mut().color = NodeColor::Red;
-												balance_tree(&mut uncle.borrow_mut().parent)
-											} else {
-												// uncle is black
-												
-												
-												
-												None
-											}
-										},
-										None => None
-									}
+									if let Some(uncle) = grandparent.borrow_mut().right.clone() {
+										if uncle.borrow_mut().color.is_red() {
+											//change to black
+											uncle.borrow_mut().color.flip_color();
+											parent.borrow_mut().color.flip_color();
+											grandparent.borrow_mut().color = NodeColor::Red;
+											return balance_tree(&mut uncle.borrow_mut().parent); // FIX
+										}
+									} 
 								} else {
-									match grandparent.borrow_mut().left.clone() {
-										Some(uncle) => {
-											if uncle.borrow_mut().color.is_red() {
-												//change to black
-												uncle.borrow_mut().color.flip_color();
-												parent.borrow_mut().color.flip_color();
-												grandparent.borrow_mut().color = NodeColor::Red;
-												balance_tree(&mut uncle.borrow_mut().parent)
-											} else {
-												None
-											}
-										},
-										None => None
+									if let Some(uncle) = grandparent.borrow_mut().left.clone() {
+										if uncle.borrow_mut().color.is_red() {
+											//change to black
+											uncle.borrow_mut().color.flip_color();
+											parent.borrow_mut().color.flip_color();
+											grandparent.borrow_mut().color = NodeColor::Red;
+											return balance_tree(&mut uncle.borrow_mut().parent); // FIX
+										}
 									}
 								}
+								let rotation = check_rotation(tree);
+
+								let mut new_tree : RedBlackTree;
+								match rotation {
+									//left left = 1 right rotate
+									1=> new_tree = right_rotate(&mut parent.borrow_mut().parent),
+									
+									2 => {
+										// left right
+										new_tree = left_rotate(&mut node.borrow_mut().parent);
+										if let Some(i) = new_tree {
+											new_tree = right_rotate(&mut i.borrow_mut().parent);
+										}
+									},
+									// right right = 1 left rotate
+									3 => new_tree = left_rotate(&mut parent.borrow_mut().parent),
+									4 => {
+										// right left
+										new_tree = right_rotate(&mut node.borrow_mut().parent);
+										if let Some(i) = new_tree {
+											new_tree = left_rotate(&mut i.borrow_mut().parent);
+										}
+									}
+									_ => panic!("Error in checking rotations")
+								}
+								balance_tree(&mut new_tree)
 							},
 							None => {
 								//parent is root
-								None
+								parent.borrow_mut().color.flip_color();
+								//panic!("Should not get here");
+								return node.borrow().parent.clone();
 							}
 						}
 					} else {
-						None
+						tree.clone()
 					}
 				},
 				None => {
-					//root
-					None
+					//tree is the root
+					node.borrow_mut().color = NodeColor::Black;
+					tree.clone()
 				}
 			}  
 		},
 		None => {
-			None
+			// inserted Node is none means there was an error in adding element into the tree
+			panic!("Error in inserting the node into the tree")
 		}
 	}
 }
