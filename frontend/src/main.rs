fa6a5ce9c701625f114f6ea3e18bb4245e36d758
naviza <43558584+naviza@users.noreply.github.com>
2022-04-04 18:47:14 -0600

    frontend starter code

---
+++ frontend/src/main.rs
@@ -0,0 +1,134 @@
+use yew::functional::*;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+mod pages;
+
+use crate::pages::Connect4Instruction::Connect4Instruction;
+
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/secure")]
+    Secure,
+    #[at("/instructions")]
+    Instructions,
+    #[at("/connect_four_vs_human")]
+    C4Human,
+    #[at("/connect_four_vs_computer")]
+    C4Computer,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+#[function_component(Secure)]
+fn secure() -> Html {
+    let history = use_history().unwrap();
+
+    let onclick_callback = Callback::from(move |_| history.push(Route::Home));
+    html! {
+        <div>
+            <h1>{ "Hey" }</h1>
+            <button onclick={onclick_callback}>{ "Go Home" }</button>
+        </div>
+    }
+}
+
+#[function_component(C4Human)]
+fn c4_against_human() -> Html {
+    let history = use_history().unwrap();
+    let onclick_callback1 = Callback::from(move |_| history.push(Route::Home));
+    html! {
+        <div>
+        <h1>{ "Connect 4 against an actual Human" }</h1>
+        <button onclick={onclick_callback1}>{ "Go to Home" }</button>
+        </div>
+    }
+}
+
+#[function_component(C4Computer)]
+fn c4_against_computer() -> Html {
+    let history = use_history().unwrap();
+    let onclick_callback1 = Callback::from(move |_| history.push(Route::Home));
+
+    html! {
+        <div>
+        <h1>{ "Connect 4 against a Computer" }</h1>
+        <button onclick={onclick_callback1}>{ "Go to Home" }</button>
+        </div>
+    }
+}
+
+#[function_component(Instructions)]
+fn instructions() -> Html {
+
+    let history = use_history().unwrap();
+    let onclick_callback1 = Callback::from(move |_| history.push(Route::Home));
+
+
+
+    html! {
+        <div>
+        <h1>{ "Instructions" }</h1>
+        <button onclick={onclick_callback1}>{ "Go to Home" }</button>
+        <Connect4Instruction />
+        </div>
+    }
+}
+
+#[function_component(Home)]
+fn home() -> Html {
+    let mut history = use_history().unwrap();
+
+    let onclick_callback1 = Callback::from(move |_| history.push(Route::Instructions));
+    history = use_history().unwrap().clone();
+    let onclick_callback2 = Callback::from(move |_| history.push(Route::C4Human));
+    history = use_history().unwrap().clone();
+    let onclick_callback3 = Callback::from(move |_| history.push(Route::C4Computer));
+    html! {
+        <div>
+            <h1>{ "Home" }</h1>
+            <div class="block-display">
+                <button onclick={onclick_callback1}>{ "Instructions for Connect 4" }</button>
+                <button onclick={onclick_callback2}>{ "Play against a Human" }</button>
+                <button onclick={onclick_callback3}>{ "Play against a Computer" }</button>
+            </div>
+        </div>
+    }
+}
+
+#[function_component(Main)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
+    }
+}
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => html! {
+            <Home />
+        },
+        Route::Secure => html! {
+            <Secure />
+        },
+        Route::Instructions => html! {
+            <Instructions />
+        },
+        Route::C4Human => html! {
+            <C4Human />
+        },
+        Route::C4Computer => html! {
+            <C4Computer />
+        },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
+
+fn main() {
+    yew::start_app::<Main>();
+}
---
+++ frontend/src/pages/Connect4Instruction.rs
@@ -0,0 +1,45 @@
+use yew::{prelude::*, html, Children, Component, Html, Properties};
+
+pub struct Connect4Instruction;
+
+
+impl Component for Connect4Instruction {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! { 
+            <>
+            <p> {"Connect Four is a two-player connection game in which the players take turns dropping colored discs from "} <br/>
+                    {"the top into a seven-column, six-row vertically suspended grid. The objective of the game is to be the "} <br/>
+                    {"first to form a horizontal, vertical, or diagonal line of four of one's own discs."}
+            </p>
+
+            <p>{"To play Connect 4 follow the following steps:"}</p>
+            <ul>
+                <li>{"A new game describes discs of which color belongs to which player"}</li>
+                <li>{"Click on the desired column on the game board to place your disc"}</li>
+                <li>{"Try to connect 4 of your colored discs either horizontally or vertically or diagonally"}{""}</li>
+            </ul>
+            
+            <p>{"For More information on Connect 4 click "}<a href="https://en.wikipedia.org/wiki/Connect_Four">{"here"}</a></p>
+            </>
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        false
+    }
+
+    fn changed(&mut self, ctx: &Context<Self>) -> bool {
+        true
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {}
+
+    fn destroy(&mut self, ctx: &Context<Self>) {}
+}
